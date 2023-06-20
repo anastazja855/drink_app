c96e9d08b46411b89b496e940025956fb80b93f6
Anastasiya Matveyenka <a.matveyenka@gmail.com>
2023-06-20 13:59:37 +0200

    Fix FavoriteDrinkScreen

--- app/src/main/java/com/example/drinkapplication/screens/favoriteDrink/FavoriteDrinkScreen.kt
+++ app/src/main/java/com/example/drinkapplication/screens/favoriteDrink/FavoriteDrinkScreen.kt
@@ -1,15 +1,24 @@
 package com.example.drinkapplication.screens
 
 import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -17,6 +26,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,55 +38,87 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.drinkapplication.model.DrinkDetailsEntity
 import com.example.drinkapplication.screens.favoriteDrink.FavoriteDrinkViewModel
+import com.example.tmsapp2.R
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
-fun FavoriteDrinkScreen(navController: NavHostController,
-                        viewModel: FavoriteDrinkViewModel = hiltViewModel()
+fun FavoriteDrinkScreen(
+    navController: NavHostController,
+    viewModel: FavoriteDrinkViewModel = hiltViewModel()
 ) {
     val isDrinkFavoriteState by viewModel.uiState.collectAsState()
 
     val favoriteDrinks by viewModel.favoriteDrinks.observeAsState(emptyList())
     Log.d("FavoriteDrinkScreen", "Favorite Drinks: $favoriteDrinks")
-    Column(modifier = Modifier.fillMaxSize()) {
-        favoriteDrinks.forEach { drink ->
-           // val isFavorite = isDrinkInFavorites(drink)
-
-            FavoriteDrinkItem(drink = drink, onDelete = { deletedDrink ->
-                viewModel.deleteFavoriteDrink(deletedDrink)
-            })
-        }
-    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.drinklist_background),
+            contentDescription = "background_image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
         LazyColumn(
             contentPadding = PaddingValues(16.dp)
         ) {
-            itemsIndexed (favoriteDrinks) { index, drink ->
-                FavoriteDrinkItem(drink){
+            itemsIndexed(favoriteDrinks) { index, drink ->
+                FavoriteDrinkItem(drink, navController) {
                     viewModel.deleteFavoriteDrink(it)
                 }
             }
         }
     }
-
+}
 
 
 @Composable
-fun FavoriteDrinkItem(drink: DrinkDetailsEntity, onDelete: (DrinkDetailsEntity) -> Unit) {
+fun FavoriteDrinkItem(drink: DrinkDetailsEntity,
+                      navController: NavHostController,
+                      onDelete: (DrinkDetailsEntity) -> Unit, ) {
+    val ctx = LocalContext.current
+    val paddind_8dp = 8.dp
 
-    Card(
+    OutlinedCard(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = 8.dp),
+            .padding(bottom = paddind_8dp)
+            .clickable { navController.navigate("drinkInfo/${drink.idDrink}" )},
+        colors = CardDefaults.outlinedCardColors(colorResource(id = R.color.orange_light).copy(0.6f)),
+        border = BorderStroke(1.dp, colorResource(id = R.color.orange))
+        //White.copy(alpha = 0.8f)
 
-    ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = drink.strDrink ?: "", style = TextStyle(fontSize = 18.sp))
-            Button(
-                onClick = { onDelete(drink) },
+        ) {
+        Row() {
+            Column(
                 modifier = Modifier
-                    .align(Alignment.End)
-                    .padding(top = 8.dp)
+                    .fillMaxWidth(0.6f)
+                    .padding(paddind_8dp)
+                    .clip(RoundedCornerShape(19.dp))
             ) {
-                Text(text = "Delete")
+                GlideImage(
+                    imageModel = drink.strDrinkThumb ?: "No photo available",
+                    contentScale = ContentScale.Inside,
+                    contentDescription = "My image",
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth(0.9f)
+                        .fillMaxHeight(0.9f)
+                )
+            }
+
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = drink.strDrink ?: "", style = TextStyle(fontSize = 18.sp))
+                Button(
+                    onClick = {
+                        val text = R.string.delete
+                        Toast.makeText(ctx, text, Toast.LENGTH_SHORT).show()
+                        onDelete(drink) },
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(top = 8.dp)
+                ) {
+                    Text(text = "Delete")
+                }
             }
         }
     }
